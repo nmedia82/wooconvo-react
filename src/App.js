da9a63dc75da92b23a7412e06d5fdee737237e84
WPBASILAHMAD <basilmedia7@gmail.com>
2022-12-14 09:52:26 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import LeftMenu from "./components/LeftMenu";
 import Admin from "./pages/Admin";
 import "./App.css";
 
-
 window.WOOCONVO_API_URL = "https://code.najeebmedia.com/wp-json/wooconvo/v1";
 
 function App() {
@@ -30,34 +29,35 @@ function App() {
     setMenuChecked(menu);
   };
   return (
-
     <div className="wooconvo-admin-wrapper">
       {/* <Admin Meta={Meta} /> */}
       {/* <Admin_react /> */}
-    
 
-    <Box sx={{ flexGrow: 1, mt: 2, ml: 2 }} className="wooconvo-admin-wrapper">
-      <Grid container spacing={2}>
-        <Grid xs={4}>
-          {/* Add Left list Items */}
-          <LeftMenu onMenuChange={handleMenuChange} />
+      <Box
+        sx={{ flexGrow: 1, mt: 2, ml: 2 }}
+        className="wooconvo-admin-wrapper"
+      >
+        <Grid container spacing={2}>
+          <Grid xs={4}>
+            {/* Add Left list Items */}
+            <LeftMenu onMenuChange={handleMenuChange} />
+          </Grid>
+          <Grid xs={8}>
+            {/* Unread ==> UnreadMessages */}
+            {MenuChecked === "unread" && <Unreads_Item />}
+
+            {/* Orders ==> Orders*/}
+            {MenuChecked === "orders" && <Orders_Item />}
+
+            {/* Starred ==> StarredOrders */}
+            {MenuChecked === "starred" && <Starred_Item />}
+            <Divider />
+
+            {/*  Settings hardcode */}
+            {MenuChecked === "settings" && <Admin Meta={Meta} />}
+          </Grid>
         </Grid>
-        <Grid xs={8}>
-          {/* Unread ==> UnreadMessages */}
-          {MenuChecked === "unread" && <Unreads_Item />}
-
-          {/* Orders ==> Orders*/}
-          {MenuChecked === "orders" && <Orders_Item />}
-
-          {/* Starred ==> StarredOrders */}
-          {MenuChecked === "starred" && <Starred_Item />}
-          <Divider />
-
-          {/*  Settings hardcode */}
-          {MenuChecked === "settings" && <Admin Meta={Meta} />}
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
     </div>
   );
 }
--- src/components/Starred.jsx
+++ src/components/Starred.jsx
@@ -1,52 +1,62 @@
-import React from 'react'
-import {Box,ListItemText,ListItemAvatar,Avatar,Typography,ListItem,Rating,Badge } from '@mui/material';
-import ImageIcon from '@mui/icons-material/Send';
-import MailIcon from '@mui/icons-material/Mail';
-import { brown  } from "@mui/material/colors";
+import React from "react";
+import {
+  Box,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+  ListItem,
+  Rating,
+  Badge,
+} from "@mui/material";
+import ImageIcon from "@mui/icons-material/Send";
+import MailIcon from "@mui/icons-material/Mail";
+import { brown } from "@mui/material/colors";
 function Starred() {
-  
   function stringAvatar(name) {
     return {
-          children: `${name.split(' ')[0][0]}`,
+      children: `${name.split(" ")[0][0]}`,
     };
   }
 
   return (
     <div>
-          <ListItem>
-          <ListItemAvatar>
-            <Avatar
-              sx={{ bgcolor: brown[500] }}
-              {...stringAvatar("Labeeb Ahmad")}
-            />
-          </ListItemAvatar>
-          <ListItemText
-           primary={
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar
+            sx={{ bgcolor: brown[500] }}
+            {...stringAvatar("Labeeb Ahmad")}
+          />
+        </ListItemAvatar>
+        <ListItemText
+          primary={
             <React.Fragment>
-              
-                <Typography
-                  sx={{ display: "inline" }}
-                  variant="h6"
-                  color="text.primary"
-                >
-                  Labeeb Ahmad
-                </Typography>
-              
+              <Typography
+                sx={{ display: "inline" }}
+                variant="h6"
+                color="text.primary"
+              >
+                Labeeb Ahmad
+              </Typography>
             </React.Fragment>
           }
-            secondary="Oct 19, 2023"
+          secondary="Oct 19, 2023"
+        />
+
+        <Box sx={{ pr: "50px" }}>
+          <Rating
+            name="customized-10"
+            defaultValue={1}
+            max={1}
+            sx={{ pr: "10px" }}
           />
-         
-          
-          <Box sx={{ pr: '50px'}}>
-         <Rating name="customized-10" defaultValue={1} max={1} sx={{ pr: '10px'}}/>
           <Badge badgeContent={4} color="primary">
             <MailIcon color="action" />
           </Badge>
-         </Box> 
-        </ListItem>
+        </Box>
+      </ListItem>
     </div>
-  )
+  );
 }
 
-export default Starred
+export default Starred;
