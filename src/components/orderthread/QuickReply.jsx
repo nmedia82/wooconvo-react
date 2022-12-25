4a595f77eaa6957769faacae06fef86166a2bdf7
WPBASILAHMAD <basilmedia7@gmail.com>
2022-12-25 19:09:16 +0500

    creating a quick reply popup

--- src/components/orderthread/QuickReply.jsx
+++ src/components/orderthread/QuickReply.jsx
@@ -1,14 +1,20 @@
 import * as React from "react";
-import { ListItemAvatar, Avatar, ListItemIcon, Tooltip } from "@mui/material/";
-import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import {
+  ListItemAvatar,
+  Avatar,
+  ListItemIcon,
+  Tooltip,
+  Dialog,
+  ListItemText,
+  ListItem,
+  List,
+  Divider,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@mui/material/";
+
 import CloseIcon from "@mui/icons-material/Close";
 import QuickreplyIcon from "@mui/icons-material/Quickreply";
 import SendIcon from "@mui/icons-material/Send";
