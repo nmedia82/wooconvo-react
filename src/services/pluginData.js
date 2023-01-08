d7a4fd845c15eece8b384a1dbd897110b9b2bb55
Najeeb Ahmad <ceo@najeebmedia.com>
2023-01-08 20:03:55 +0500

    aws done

--- src/services/pluginData.js
+++ src/services/pluginData.js
@@ -5,36 +5,36 @@
  * 3. myaccount
  * 4. dokan_vendor
  */
-window.WOOCONVO_Data = JSON.stringify({
-  plugin_url:
-    "https://wooconvo.najeebmedia.com/wp-content/plugins/nm-wooconvo-v8",
-  api_url: "https://wooconvo.najeebmedia.com/wp-json/wooconvo/v1",
-  user_id: 1,
-  context: "wp_admin",
-  settings: {
-    message_vs_order_status: "wc-pending",
-    enable_msg_count_display: true,
-    enable_msg_search: true,
-    myaccount_tab_label: "My messages",
-    bg_color_message_header: "#e1d5d5",
-    bg_color_order_messages: "#9fc7df",
-    enable_order_notices: true,
-    reverse_message_display_order: false,
-    enable_file_attachments: true,
-    max_files_allowed: 2,
-    max_file_size: 500,
-    file_types_allowed: "jpg,png,pdf,zip",
-    attachments_required: false,
-    attachments_in_email: true,
-    image_open_click: true,
-    enable_quickreply: false,
-    enable_aws: true,
-    aws_accesskey: "xxxxxxx",
-    aws_secret: "xxxxx/xs6CUf",
-    aws_region: "us-east-1",
-    aws_bucket: "nmedia-public",
-  },
-});
+// window.WOOCONVO_Data = JSON.stringify({
+//   plugin_url:
+//     "https://wooconvo.najeebmedia.com/wp-content/plugins/nm-wooconvo-v8",
+//   api_url: "https://wooconvo.najeebmedia.com/wp-json/wooconvo/v1",
+//   user_id: 1,
+//   context: "wp_admin",
+//   settings: {
+//     message_vs_order_status: "wc-pending",
+//     enable_msg_count_display: true,
+//     enable_msg_search: true,
+//     myaccount_tab_label: "My messages",
+//     bg_color_message_header: "#e1d5d5",
+//     bg_color_order_messages: "#9fc7df",
+//     enable_order_notices: true,
+//     reverse_message_display_order: false,
+//     enable_file_attachments: true,
+//     max_files_allowed: 2,
+//     max_file_size: 500,
+//     file_types_allowed: "jpg,png,pdf,zip",
+//     attachments_required: false,
+//     attachments_in_email: true,
+//     image_open_click: true,
+//     enable_quickreply: false,
+//     enable_aws: true,
+//     aws_accesskey: "xxxxxxx",
+//     aws_secret: "xxxxx/xs6CUf",
+//     aws_region: "us-east-1",
+//     aws_bucket: "nmedia-public",
+//   },
+// });
 
 // window.WOOCONVO_Data = JSON.stringify({
 //   plugin_url:
