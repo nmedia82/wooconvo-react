5f9c357d55c4056a704f8c54c8f1ad9862b46bd2
CEO-NMEDIA <ceo@najeebmedia.com>
2023-02-26 14:16:15 +0500

    some settings

--- src/components/orderthread/ReplyMsg.jsx
+++ src/components/orderthread/ReplyMsg.jsx
@@ -76,6 +76,8 @@ export default function ReplyMsg({ onReplySend }) {
   };
 
   const validateAttachments = () => {
+    const attachment_enabled = get_setting("enable_file_attachments");
+    if (!attachment_enabled) return false;
     const attach_required = get_setting("attachments_required");
     if (attach_required && !Files.length) return true;
     return false;
@@ -93,7 +95,9 @@ export default function ReplyMsg({ onReplySend }) {
         component="form"
         sx={{ p: "2px 4px", display: "flex", bgcolor: common }}
       >
-        <Attachments onFileSelected={handleFileSelected} />
+        {get_setting("enable_file_attachments") && (
+          <Attachments onFileSelected={handleFileSelected} />
+        )}
 
         <TextField
           value={ReplyText}
